bc7c43530600f86993dcfe3fec5d9cefe7a16650
Shipan Ahmed <shipanahmed1999@gmail.com>
2026-06-29 15:36:39 +0600

    added api books

--- index.js
+++ index.js
@@ -33,7 +33,20 @@ async function run() {
         const booksCollection = database.collection("books");
         const paymentCollection = database.collection("payments");
         const reviewsCollection = database.collection("reviews");
+        const usersCollection = database.collection("user");
 
+        /// ======================= user api =====================
+        app.get('/api/users', async (req, res) => {
+            try {
+                const users = await usersCollection.find({}).toArray();
+                res.status(200).json(users);
+            } catch (error) {
+                console.error('Error fetching users:', error);
+                res.status(500).json({ message: 'Internal server error' });
+            }
+        });
+
+        
         ///  ======================= review api =====================
 
         // patch review by reviewid
